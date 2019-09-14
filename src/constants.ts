54440aafa7b93d019ea877da6659ce95169d67a3
Agney <agney@outlook.in>
2019-09-14 09:56:35 +0530

    prerequisites

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+const GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
+const GITHUB_EVENT_NAME = process.env["GITHUB_EVENT_NAME"];
+
+export {
+  GITHUB_TOKEN,
+  GITHUB_EVENT_NAME,
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import * as core from '@actions/core';
 
+import { GITHUB_TOKEN, GITHUB_EVENT_NAME } from "./constants";
+
+if (!GITHUB_TOKEN) {
+  console.log("You must enable the GITHUB_TOKEN secret");
+  process.exit(1);
+}
+
 async function run() {
+
+  // Bail out if the event that executed the action wasn’t a pull_request
+  if (GITHUB_EVENT_NAME !== "pull_request") {
+    console.log("This action only runs for pushes to PRs");
+    process.exit(78);
+  }
+  
   try {
     const myInput = core.getInput('myInput');
     core.debug(`Hello ${myInput}`);
