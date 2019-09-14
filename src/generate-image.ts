7bca0509df09f009fbd73fd0d5d3ccfce0e13577
Agney <agney@outlook.in>
2019-09-15 01:58:41 +0530

    Add full path for chrome in docker container

--- src/generate-image.ts
+++ src/generate-image.ts
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer-core";
 import { REPO_DIRECTORY } from "./constants";
 
 async function generateImage(html: string = `<h1>Default content</h1>`) {
-  const browser = await puppeteer.launch({ executablePath: 'google-chrome-unstable' }) ;
+  const browser = await puppeteer.launch({ executablePath: '/usr/bin/google-chrome-unstable' }) ;
   const page = await browser.newPage();
   await page.setContent(html);
   const image = await page.screenshot({ encoding: "base64" });
