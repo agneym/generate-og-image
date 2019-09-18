dad278238cb3e917454b835cb7d83c0f089d0050
Agney <agney@outlook.in>
2019-09-19 00:52:43 +0530

    change styles a bit

--- src/generate-html.ts
+++ src/generate-html.ts
@@ -8,14 +8,14 @@ function generateHtml(prop: Partial<IRepoProps>) {
     <html lang="en-GB">
     <head>
       <meta charset="utf-8">
-      <link href="https://fonts.googleapis.com/css?family=Aleo|Open+Sans&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Nunito:600|Open+Sans&display=swap" rel="stylesheet">
       <style>
         body {
           margin: 0;
           font-family: 'Open Sans', sans-serif;
         }
         og-image-element {
-          --heading-font: 'Aleo', serif;
+          --heading-font: 'Nunito', serif;
           --background: ${prop.backgroundColor};
           --font-color: ${prop.fontColor};
         }
